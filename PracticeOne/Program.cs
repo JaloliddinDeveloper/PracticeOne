7ced53338388a87e55bea96fc8c2d4708ccf04c8
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-06-12 12:57:47 +0500

    HomeTask 1

---
+++ PracticeOne/Program.cs
@@ -0,0 +1,7 @@
+﻿//HomeTask 1
+using PracticeOne.Classes;
+
+var rect = new Rectangle(10, 5, "Blue");
+Console.WriteLine($"Area: {rect.GetArea()}");         
+Console.WriteLine($"Perimeter: {rect.GetPerimeter()}"); 
+Console.WriteLine($"Color: {rect.Color}");          
