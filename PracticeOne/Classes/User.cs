178f2f2bce368f109cd41e4f6eb10c2d1370e9c6
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-06-12 13:02:15 +0500

    HomeTask 2

---
+++ PracticeOne/Classes/User.cs
@@ -0,0 +1,43 @@
+﻿namespace PracticeOne.Classes
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsLoggedIn { get; private set; }
+
+        public User(string firstName, string lastName, string username, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Username = username;
+            Password = password;
+            IsLoggedIn = false;
+        }
+
+        public void Login(string username, string password)
+        {
+            if (Username == username && Password == password)
+            {
+                IsLoggedIn = true;
+                Console.WriteLine("User logged in successfully");
+            }
+            else
+            {
+                Console.WriteLine("Your password or username is incorrect");
+            }
+        }
+
+        public void Logout()
+        {
+            IsLoggedIn = false;
+        }
+
+        public string GetFullInfo()
+        {
+            return $"{FirstName} {LastName} - {IsLoggedIn.ToString().ToLower()}";
+        }
+    }
+}
--- PracticeOne/Program.cs
+++ PracticeOne/Program.cs
@@ -1,7 +1,21 @@
 ﻿//HomeTask 1
+//using PracticeOne.Classes;
+
+//var rect = new Rectangle(10, 5, "Blue");
+//Console.WriteLine($"Area: {rect.GetArea()}");         
+//Console.WriteLine($"Perimeter: {rect.GetPerimeter()}"); 
+//Console.WriteLine($"Color: {rect.Color}");          
+
+//HomeTask 2
+
 using PracticeOne.Classes;
 
-var rect = new Rectangle(10, 5, "Blue");
-Console.WriteLine($"Area: {rect.GetArea()}");         
-Console.WriteLine($"Perimeter: {rect.GetPerimeter()}"); 
-Console.WriteLine($"Color: {rect.Color}");          
+var user = new User("Jaloliddin", "Mahkamov", "jaloliddin1115", "mypassword");
+
+user.Login("jaloliddin1115", "mypassword");
+
+Console.WriteLine(user.GetFullInfo());
+
+user.Logout();
+
+Console.WriteLine(user.GetFullInfo());
